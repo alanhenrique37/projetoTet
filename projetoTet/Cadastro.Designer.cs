c08f5e522d0c53611835a180199ede63196f82e2
ALAN HENRIQUE SILVA DE ABREU <alan.hsabreu@senacsp.edu.br>
2025-06-06 17:09:46 -0300

    segundo commit

--- projetoTet/Cadastro.Designer.cs
+++ projetoTet/Cadastro.Designer.cs
@@ -32,7 +32,18 @@
             pictureBox1 = new PictureBox();
             listView1 = new ListView();
             label1 = new Label();
+            textBox1 = new TextBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            textBox2 = new TextBox();
+            pictureBox4 = new PictureBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -59,20 +70,118 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Calibri", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.Transparent;
-            label1.Location = new Point(764, 144);
+            label1.BackColor = Color.FromArgb(118, 75, 162);
+            label1.Font = new Font("Segoe UI Semibold", 44.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(699, 129);
             label1.Name = "label1";
-            label1.Size = new Size(122, 36);
+            label1.Size = new Size(270, 78);
             label1.TabIndex = 2;
             label1.Text = "Cadastro";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(596, 247);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(469, 35);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(118, 75, 162);
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(539, 243);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(39, 43);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(118, 75, 162);
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox3.Location = new Point(541, 312);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(35, 44);
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(596, 314);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(469, 35);
+            textBox2.TabIndex = 7;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.FromArgb(118, 75, 162);
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox4.Location = new Point(540, 383);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(39, 39);
+            pictureBox4.TabIndex = 8;
+            pictureBox4.TabStop = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(596, 384);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(469, 35);
+            textBox3.TabIndex = 9;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(118, 100, 162);
+            button1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.WhiteSmoke;
+            button1.Location = new Point(715, 451);
+            button1.Name = "button1";
+            button1.Size = new Size(224, 45);
+            button1.TabIndex = 10;
+            button1.Text = "Cadastrar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.ActiveLinkColor = Color.FromArgb(118, 100, 162);
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.FromArgb(118, 100, 162);
+            linkLabel1.DisabledLinkColor = Color.FromArgb(118, 100, 162);
+            linkLabel1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            linkLabel1.LinkColor = Color.White;
+            linkLabel1.Location = new Point(749, 572);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(0, 32);
+            linkLabel1.TabIndex = 12;
+            linkLabel1.VisitedLinkColor = Color.FromArgb(118, 100, 162);
             // 
             // Cadastro
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1250, 736);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(pictureBox4);
+            Controls.Add(textBox2);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
             Controls.Add(listView1);
@@ -80,6 +189,9 @@
             Text = "Cadastro";
             Load += Cadastro_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -89,5 +201,13 @@
         private PictureBox pictureBox1;
         private ListView listView1;
         private Label label1;
+        private TextBox textBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private TextBox textBox2;
+        private PictureBox pictureBox4;
+        private TextBox textBox3;
+        private Button button1;
+        private LinkLabel linkLabel1;
     }
 }
--- projetoTet/Cadastro.cs
+++ projetoTet/Cadastro.cs
@@ -1,33 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace projetoTet
 {
     public partial class Cadastro : Form
     {
+        private Label placeholderLabel1 = null!;
+        private Label placeholderLabel2 = null!;
+        private Label placeholderLabel3 = null!;
+
+        // Cores do botão iguais ao Form1
+        private Color normalColor = Color.MediumSlateBlue;
+        private Color hoverColor = Color.MediumPurple;
+        private Color clickColor = Color.Indigo;
+
         public Cadastro()
         {
             InitializeComponent();
+
+            textBox1.Enter += textBox_Enter;
+            textBox1.Leave += textBox_Leave;
+            textBox1.TextChanged += textBox_TextChanged;
+
+            textBox2.Enter += textBox_Enter;
+            textBox2.Leave += textBox_Leave;
+            textBox2.TextChanged += textBox_TextChanged;
+
+            textBox3.Enter += textBox_Enter;
+            textBox3.Leave += textBox_Leave;
+            textBox3.TextChanged += textBox_TextChanged;
+
+            // Configuração do botão igual ao Form1
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.BackColor = normalColor;
+            button1.ForeColor = Color.White;
+            button1.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+            button1.TabStop = false;
+
+            button1.Region = new Region(GetRoundedRectPath(button1.ClientRectangle, 10));
+
+            button1.MouseEnter += Button1_MouseEnter;
+            button1.MouseLeave += Button1_MouseLeave;
+            button1.MouseDown += Button1_MouseDown;
+            button1.MouseUp += Button1_MouseUp;
+            button1.Resize += Button1_Resize;
         }
 
         private void Cadastro_Load(object sender, EventArgs e)
         {
-            ArredondarPictureBox(pictureBox1, 30); // raio 20, ajuste se quiser
-            ArredondarListView(listView1, 30); // raio 20, ajuste se quiser
+            ArredondarPictureBox(pictureBox1, 30);
+            ArredondarListView(listView1, 30);
+
+            textBox1.Multiline = true;
+            textBox1.Height = 40;
+            textBox2.Multiline = true;
+            textBox2.Height = 40;
+            textBox3.Multiline = true;
+            textBox3.Height = 40;
+
+            ArredondarTextBox(textBox1, 20);
+            ArredondarTextBox(textBox2, 20);
+            ArredondarTextBox(textBox3, 20);
+
+            CriarPlaceholderCustomizado();
         }
-        // Gradiente no fundo da janela
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void textBox3_TextChanged(object sender, EventArgs e) { }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-
             Graphics g = e.Graphics;
             Rectangle rect = this.ClientRectangle;
 
@@ -41,10 +88,6 @@ namespace projetoTet
             }
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
         private void ArredondarPictureBox(PictureBox pb, int raio)
         {
             GraphicsPath path = new GraphicsPath();
@@ -56,6 +99,7 @@ namespace projetoTet
             path.CloseFigure();
             pb.Region = new Region(path);
         }
+
         private void ArredondarListView(ListView lv, int raio)
         {
             GraphicsPath path = new GraphicsPath();
@@ -68,11 +112,117 @@ namespace projetoTet
             lv.Region = new Region(path);
         }
 
+        private void ArredondarTextBox(TextBox tb, int raio)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(0, 0, raio, raio, 180, 90);
+            path.AddArc(tb.Width - raio, 0, raio, raio, 270, 90);
+            path.AddArc(tb.Width - raio, tb.Height - raio, raio, raio, 0, 90);
+            path.AddArc(0, tb.Height - raio, raio, raio, 90, 90);
+            path.CloseFigure();
+            tb.Region = new Region(path);
+        }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        private void CriarPlaceholderCustomizado()
+        {
+            placeholderLabel1 = CriarLabelPlaceholder(textBox1, "Digite seu nome");
+            placeholderLabel2 = CriarLabelPlaceholder(textBox2, "Digite seu email");
+            placeholderLabel3 = CriarLabelPlaceholder(textBox3, "Digite sua senha");
+
+            AtualizarPlaceholders();
+        }
+
+        private Label CriarLabelPlaceholder(TextBox textBox, string texto)
+        {
+            Label label = new Label
+            {
+                Text = texto,
+                ForeColor = Color.Gray,
+                Font = new Font(textBox.Font.FontFamily, 9, FontStyle.Italic),
+                BackColor = Color.Transparent,
+                AutoSize = false,
+                TextAlign = ContentAlignment.MiddleLeft
+            };
+
+            label.SetBounds(5, 3, textBox.Width - 10, textBox.Height - 6);
+            label.Click += (s, e) => { textBox.Focus(); };
+            textBox.Controls.Add(label);
+            label.BringToFront();
+
+            return label;
+        }
+
+        private void AtualizarPlaceholders()
+        {
+            placeholderLabel1.Visible = string.IsNullOrWhiteSpace(textBox1.Text) && !textBox1.Focused;
+            placeholderLabel2.Visible = string.IsNullOrWhiteSpace(textBox2.Text) && !textBox2.Focused;
+            placeholderLabel3.Visible = string.IsNullOrWhiteSpace(textBox3.Text) && !textBox3.Focused;
+        }
+
+        private void textBox_Enter(object sender, EventArgs e) => AtualizarPlaceholders();
+        private void textBox_Leave(object sender, EventArgs e) => AtualizarPlaceholders();
+        private void textBox_TextChanged(object sender, EventArgs e) => AtualizarPlaceholders();
+
+        // Botão arredondado com hover igual ao Form1
+
+        private GraphicsPath GetRoundedRectPath(Rectangle bounds, int radius)
+        {
+            int diameter = radius * 2;
+            GraphicsPath path = new GraphicsPath();
+
+            path.StartFigure();
+            path.AddArc(bounds.X, bounds.Y, diameter, diameter, 180, 90);
+            path.AddArc(bounds.Right - diameter, bounds.Y, diameter, diameter, 270, 90);
+            path.AddArc(bounds.Right - diameter, bounds.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(bounds.X, bounds.Bottom - diameter, diameter, diameter, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = hoverColor;
+        }
+
+        private void Button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = normalColor;
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = clickColor;
+        }
+
+        private void Button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (button1.ClientRectangle.Contains(button1.PointToClient(Cursor.Position)))
+            {
+                button1.BackColor = hoverColor;
+            }
+            else
+            {
+                button1.BackColor = normalColor;
+            }
+        }
+
+        private void Button1_Resize(object sender, EventArgs e)
+        {
+            button1.Region = new Region(GetRoundedRectPath(button1.ClientRectangle, 10));
+        }
+
+        // Outros eventos opcionais
+        private void pictureBox1_Click(object sender, EventArgs e) { }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void pictureBox2_Click(object sender, EventArgs e) { }
+        private void button1_Click(object sender, EventArgs e) { }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
     }
 }
-
